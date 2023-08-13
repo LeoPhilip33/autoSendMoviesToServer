eb296c15e359a46e17c520898b2fa308ac0d8dfd
LeoPhilip33 <leo.philip33.bordeaux@gmail.com>
2023-08-13 19:07:47 +0200

    feat: first commit !

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+hostname = 'XXX.XXX.X.XX'
+port = 22 # default SSH port
+username = 'yourUsername'
+password = 'yourPassword'
+timeout= 150 # SSH connection timeout
+
+localPath = "H:\Film\Compresse" # Local folder where he has your movies to send
+remotePath = '/mnt/hddTerminator/plex/Media/Movies' # Remote folder that will receive your movies
---
+++ index.py
@@ -0,0 +1,67 @@
+import os
+import glob
+import paramiko
+from scp import SCPClient
+from send2trash import send2trash
+import config
+from tqdm import tqdm
+
+def sendFilm(movie):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(
+        config.hostname,
+        port=config.port,
+        username=config.username,
+        password=config.password,
+        timeout=config.timeout
+    )
+    
+    nameMovie = os.path.basename(movie)
+    localPath = os.path.join(config.localPath, nameMovie)
+    remotePath = config.remotePath + '/' + nameMovie
+    print(f"{nameMovie} movie Transfer")
+    
+    try:
+        with ssh.open_sftp() as sftp:
+            sftp.listdir(config.remotePath)
+    except Exception as e:
+        print(f"Error accessing remote folder: {e}")
+    
+    try:
+        total_size = os.path.getsize(localPath)
+        
+        with tqdm(total=total_size, unit='B', unit_scale=True, miniters=1, desc=f"Transferring {nameMovie}") as pbar:
+            def progress(filename, size, sent):
+                pbar.update(sent - pbar.n)
+            
+            with SCPClient(ssh.get_transport(), progress=progress) as scp:
+                scp.put(localPath, remotePath)
+        
+        print(f"Transfer completed for {nameMovie} movie.")
+        
+        send2trash(localPath)
+        print(f"The movie {nameMovie} was trashed.")
+    
+    except Exception as e:
+        print(f"Error transferring {nameMovie} movie. {e}")
+    
+    ssh.close()
+
+
+movies = sorted(glob.glob(os.path.join(config.localPath, "*.mp4")))
+lastElement = movies.pop()
+
+print(f"{len(movies)} movies found.")
+print("1. Send all available movies")
+print("2. Exit")
+choice = input("Enter your choice: ")
+
+if choice == '1':
+    for movie in movies:
+        sendFilm(movie)
+    
+elif choice == '2':
+    print('Exiting...')
+else:
+    print('Invalid choice.')
